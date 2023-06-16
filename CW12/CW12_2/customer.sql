647feed4a7e9c7db6d37753ed5a4f2e7c87efc7f
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-06-16 23:03:24 +0330

    small change

--- CW12/CW12_2/customer.sql
+++ CW12/CW12_2/customer.sql
@@ -1,11 +1,11 @@
 ---part1
--- SELECT first_name, last_name FROM customer WHERE first_name ILIKE 'JEN%';
+SELECT first_name, last_name FROM customer WHERE first_name ILIKE 'JEN%';
 
--- ---part2
--- SELECT first_name, last_name FROM customer WHERE first_name ILIKE '%ER%';
+---part2
+SELECT first_name, last_name FROM customer WHERE first_name ILIKE '%ER%';
 
--- ---part3
--- SELECT first_name, last_name FROM customer WHERE first_name ILIKE '%l';
+---part3
+SELECT first_name, last_name FROM customer WHERE first_name ILIKE '%l';
 
 ---part4
 SELECT first_name, last_name FROM customer WHERE first_name NOT ILIKE 'JEN%'
