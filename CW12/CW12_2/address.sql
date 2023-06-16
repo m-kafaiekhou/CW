2be6561aa6b7d1409f2e823581cf1967260cf738
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-06-16 22:32:08 +0330

    add part5 CW12

--- CW12/CW12_2/address.sql
+++ CW12/CW12_2/address.sql
@@ -25,19 +25,23 @@
 -- WHERE scanner.first_rows IN (1, 2, 3) OR scanner.last_rows IN (1, 2, 3) ORDER BY cnt;
 
 ---- counts how many addresses(address or address2) are in one district(one query, top3 and bottom 3)
-WITH scanner AS (
-    SELECT DISTINCT district, 
-           COALESCE(COUNT(NULLIF(address, ''))) + COALESCE(COUNT(NULLIF(address2, ''))) AS cnt,
-           ROW_NUMBER() OVER (ORDER BY COUNT(*) ASC) AS first_rows, 
-           ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS last_rows
-    FROM address
-    GROUP BY district
-)
+-- WITH scanner AS (
+--     SELECT DISTINCT district, 
+--            COALESCE(COUNT(NULLIF(address, ''))) + COALESCE(COUNT(NULLIF(address2, ''))) AS cnt,
+--            ROW_NUMBER() OVER (ORDER BY COUNT(*) ASC) AS first_rows, 
+--            ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS last_rows
+--     FROM address
+--     GROUP BY district
+-- )
 
-SELECT district, cnt 
-FROM scanner 
-WHERE scanner.first_rows IN (1, 2, 3) OR scanner.last_rows IN (1, 2, 3) 
-ORDER BY cnt;
+-- SELECT district, cnt 
+-- FROM scanner 
+-- WHERE scanner.first_rows IN (1, 2, 3) OR scanner.last_rows IN (1, 2, 3) 
+-- ORDER BY cnt;
 
 ---part4
 -- SELECT address, address2, district FROM address WHERE district IN ('California', 'Alberta');
+
+---part5
+SELECT address, address2, district FROM address WHERE district IN 
+('California', 'Alberta', 'Texas', 'Hamilton');
