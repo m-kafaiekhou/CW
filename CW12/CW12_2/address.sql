0c1a47e727368d7b95d0820bc46de1af983355e0
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-06-16 21:23:54 +0330

    add uncomplete query on address

--- CW12/CW12_2/address.sql
+++ CW12/CW12_2/address.sql
@@ -1,3 +1,30 @@
 --SELECT DISTINCT district FROM address ORDER BY district LIMIT 5;
 
-SELECT COUNT(*) FROM (SELECT DISTINCT district FROM address) AS unq;
+--SELECT COUNT(*) FROM (SELECT DISTINCT district FROM address) AS unq; 
+
+
+-- (SELECT DISTINCT district, COUNT(district) AS cnt FROM address GROUP BY district ORDER BY cnt LIMIT 3)
+-- UNION ALL
+-- (SELECT DISTINCT district, COUNT(district) AS cnt FROM address GROUP BY district ORDER BY cnt DESC LIMIT 3);
+
+-- WITH scanner AS (
+--     SELECT DISTINCT district, COUNT(district), ROW_NUMBER() OVER (
+--         ORDER  BY COUNT(district) ASC) AS first_rows, ROW_NUMBER() OVER
+--          (ORDER  BY COUNT(district) DESC) AS last_rows
+-- FROM address)
+
+-- SELECT district, cnt FROM scanner 
+-- WHERE scanner.first_rows IN (1 OR 2 OR 3) OR 
+-- scanner.last_rows IN (1 OR 2 OR 3);
+
+WITH scanner AS (
+    SELECT DISTINCT district, COUNT(*) AS cnt, 
+           ROW_NUMBER() OVER (ORDER BY COUNT(*) ASC) AS first_rows, 
+           ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS last_rows
+    FROM address
+    GROUP BY district
+)
+
+SELECT district, cnt 
+FROM scanner 
+WHERE scanner.first_rows IN (1, 2, 3) OR scanner.last_rows IN (1, 2, 3) ORDER BY cnt;
--- CW12/CW12_2/dvd.sql
+++ CW12/CW12_2/dvd.sql
@@ -1,13 +1,13 @@
--- SELECT * FROM film WHERE rental_duration > 4;
+SELECT * FROM film WHERE rental_duration > 4;  -- 1
 
--- SELECT * FROM film WHERE rental_duration > 2 AND rental_duration < 5;
+SELECT * FROM film WHERE rental_duration > 2 AND rental_duration < 5;  -- 2
 
--- SELECT * FROM film WHERE rental_duration > 2 AND rental_duration < 5 ORDER BY last_update ASC;
+SELECT * FROM film WHERE rental_duration > 2 AND rental_duration < 5 ORDER BY last_update ASC; --3
 
--- SELECT * FROM film WHERE rental_duration > 2 AND rental_duration < 5 ORDER BY last_update DESC;
+SELECT * FROM film WHERE rental_duration > 2 AND rental_duration < 5 ORDER BY last_update DESC; --3
 
--- SELECT * FROM film WHERE rental_duration > 4 ORDER BY title ASC;
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY title ASC; -- 3
 
--- SELECT * FROM film WHERE rental_duration > 4 ORDER BY title DESC;
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY title DESC; -- 3
 
-SELECT ROUND(AVG(length)), MAX(length), MIN(length) FROM film;
+SELECT ROUND(AVG(length)), MAX(length), MIN(length) FROM film; -- 4
