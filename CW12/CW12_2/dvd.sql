e4fd6e8a4977ba4e9f05f256b2dff038bd190ff5
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-06-16 15:24:54 +0330

    first question done

--- CW12/CW12_2/dvd.sql
+++ CW12/CW12_2/dvd.sql
@@ -1,11 +1,13 @@
-SELECT * FROM film WHERE rental_duration > 4;
+-- SELECT * FROM film WHERE rental_duration > 4;
 
-SELECT * FROM film WHERE rental_duration > 2 AND rental_duration < 5;
+-- SELECT * FROM film WHERE rental_duration > 2 AND rental_duration < 5;
 
-SELECT * FROM film WHERE rental_duration > 2 AND rental_duration < 5 ORDER BY last_update ASC;
+-- SELECT * FROM film WHERE rental_duration > 2 AND rental_duration < 5 ORDER BY last_update ASC;
 
-SELECT * FROM film WHERE rental_duration > 2 AND rental_duration < 5 ORDER BY last_update DESC;
+-- SELECT * FROM film WHERE rental_duration > 2 AND rental_duration < 5 ORDER BY last_update DESC;
 
-SELECT * FROM film WHERE rental_duration > 4 ORDER BY title ASC;
+-- SELECT * FROM film WHERE rental_duration > 4 ORDER BY title ASC;
 
-SELECT * FROM film WHERE rental_duration > 4 ORDER BY title DESC;
+-- SELECT * FROM film WHERE rental_duration > 4 ORDER BY title DESC;
+
+SELECT ROUND(AVG(length)), MAX(length), MIN(length) FROM film;
