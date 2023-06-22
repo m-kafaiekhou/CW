911f35cf61f77486582c744fedf612d64ea544f1
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-06-22 14:28:14 +0330

    CW13 q1 p1

---
+++ CW13/1.sql
@@ -0,0 +1,4 @@
+SELECT film.title, category.name as category
+FROM film
+join film_category on film_category.film_id = film.film_id
+join category on category.category_id = film_category.category_id;
