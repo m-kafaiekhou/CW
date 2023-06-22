d2edb2bb42fe1a0d7bdcdf0fba8cea252dca0977
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-06-22 14:42:24 +0330

    CW13 q1 p3

--- CW13/1.sql
+++ CW13/1.sql
@@ -4,8 +4,16 @@
 -- join category on category.category_id = film_category.category_id;
 
 
-SELECT film.title, category.name as category, film.release_year 
+-- SELECT film.title, category.name as category, film.release_year 
+-- FROM film
+-- join film_category on film_category.film_id = film.film_id
+-- join category on category.category_id = film_category.category_id
+-- WHERE category.name IN ('Action', 'Comedy', 'Family'); 
+
+
+SELECT category.name, COUNT(*) as count
 FROM film
 join film_category on film_category.film_id = film.film_id
 join category on category.category_id = film_category.category_id
-WHERE category.name IN ('Action', 'Comedy', 'Family');
+GROUP BY category.name
+ORDER BY count DESC;
