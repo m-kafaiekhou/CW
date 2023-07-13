433453b94770ec0f13a1e7779a7c743c4a6ab153
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-07-13 16:34:35 +0330

    changed front add post list

--- CW16/BlogPlatform/blog/urls.py
+++ CW16/BlogPlatform/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import home_page_view
+from .views import home_page_view, post_list_view
 
 
 urlpatterns = [
     path('', home_page_view, name='home'),
+    path('posts/', post_list_view, name='posts_list'),
 ]
--- CW16/BlogPlatform/blog/views.py
+++ CW16/BlogPlatform/blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 
 def home_page_view(request):
-    return render(request, 'blog/home1.html')
+    return render(request, 'blog/home.html')
+
+
+def post_list_view(request):
+    posts_list = Post.objects.filter(status='pub').order_by('-datetime_modified')
+    return render(request, 'blog/posts_list.html', {'posts_list': posts_list})
