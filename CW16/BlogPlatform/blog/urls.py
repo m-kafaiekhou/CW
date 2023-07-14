abc99873d63eff15b114f3b55f88fb9d23afb60d
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-07-14 10:15:31 +0330

    fixed lists description limit

--- CW16/BlogPlatform/blog/urls.py
+++ CW16/BlogPlatform/blog/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
-from .views import home_page_view, post_list_view, categories_list_view, post_detail_view, category_detail_view
-
+from . import views
 
 urlpatterns = [
-    path('', home_page_view, name='home'),
-    path('posts/', post_list_view, name='posts_list'),
-    path('categories/', categories_list_view, name='categories_list'),
-    path('posts/<int:pk>/', post_detail_view, name='post_detail'),
-    path('categories/<int:pk>/', category_detail_view, name='category_detail'),
+    path('', views.home_page_view, name='home'),
+    path('posts/', views.post_list_view, name='posts_list'),
+    path('categories/', views.categories_list_view, name='categories_list'),
+    path('posts/<int:pk>/', views.post_detail_view, name='post_detail'),
+    path('categories/<int:pk>/', views.category_detail_view, name='category_detail'),
 ]
