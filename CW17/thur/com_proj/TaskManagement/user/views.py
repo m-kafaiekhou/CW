f197b310f63a82c4a4b1719def59050a17130c34
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-04 08:53:32 +0330

    chore: sign up view

--- CW17/thur/com_proj/TaskManagement/user/views.py
+++ CW17/thur/com_proj/TaskManagement/user/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+        return render(request, 'registration/signup.html', context={'form': form})
