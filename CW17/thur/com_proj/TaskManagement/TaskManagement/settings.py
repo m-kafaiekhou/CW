851ebd92586e2c42bba134f4457e470105de6baa
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-04 05:25:52 +0330

    custom user model

--- CW17/thur/com_proj/TaskManagement/TaskManagement/settings.py
+++ CW17/thur/com_proj/TaskManagement/TaskManagement/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/media'
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
 
 # Default primary key field type
--- CW17/thur/com_proj/TaskManagement/user/models.py
+++ CW17/thur/com_proj/TaskManagement/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    photo = models.ImageField(upload_to='images/', null=True, blank=True)
