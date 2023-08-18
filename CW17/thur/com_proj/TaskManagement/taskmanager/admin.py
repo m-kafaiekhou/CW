c8f2c0292066b1561f4d71350ab111bcace22700
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-18 12:18:40 +0330

    chore: export to json

--- CW17/thur/com_proj/TaskManagement/taskmanager/admin.py
+++ CW17/thur/com_proj/TaskManagement/taskmanager/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.shortcuts import HttpResponse
 from .models import Task, Tag, Category, Note
 import csv
+import json
+from django.http import JsonResponse
 
 
 class ExportCsvMixin:
@@ -23,11 +25,31 @@ class ExportCsvMixin:
     export_as_csv.short_description = "Export Selected"
 
 
+class ExportJsonMixin:
+    def export_as_json(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        j = {}
+
+        for i, obj in enumerate(queryset):
+            row = {field: getattr(obj, field).__str__() for field in field_names}
+            j[i] = row
+
+        response = JsonResponse(j, content_type='text/json')
+        response['Content-Disposition'] = 'attachment; filename={}.json'.format(meta)
+
+        return response
+
+    export_as_json.short_description = "Export Json"
+
+
 @admin.register(Task)
-class TaskAdmin(admin.ModelAdmin, ExportCsvMixin):
+class TaskAdmin(admin.ModelAdmin, ExportCsvMixin, ExportJsonMixin):
     list_per_page = 20
     list_display = ['title', 'status', 'category']
-    actions = ['export_as_csv']
+    actions = ['export_as_csv', 'export_as_json']
     readonly_fields = ['created_at', 'updated', 'is_active']
 
 
